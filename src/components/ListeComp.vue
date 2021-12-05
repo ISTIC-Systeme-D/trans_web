43e1fe193b2d6913516d83cef5ab7323dc0a9e6a
Lohier Anthony <anthony.lohier@etudiant.univ-rennes1.fr>
2021-12-05 16:46:12 +0100

    filter table

--- src/components/ListeComp.vue
+++ src/components/ListeComp.vue
@@ -1,55 +1,35 @@
 <template>
+  <div style="text-align: center">
+    <label>SearchBy:</label><input v-model="searchTerm" />
+  </div>
   <table-lite
-    :is-loading="table.isLoading"
+    :is-static-mode="true"
     :columns="table.columns"
     :rows="table.rows"
     :total="table.totalRecordCount"
     :sortable="table.sortable"
-    :messages="table.messages"
-    @do-search="doSearch"
-    @is-finished="table.isLoading = false"
   ></table-lite>
 </template>
 
 <script>
-import { defineComponent, reactive } from "vue";
-import TableLite from 'vue3-table-lite';
-// Fake Data for 'asc' sortable
-const sampleData1 = (offst, limit) => {
-  offst = offst + 1;
-  let data = [];
-  for (let i = offst; i <= limit; i++) {
-    data.push({
-      id: i,
-      name: "TEST" + i,
-      email: "test" + i + "@example.com",
-    });
-  }
-  return data;
-};
-// Fake Data for 'desc' sortable
-const sampleData2 = (offst, limit) => {
-  let data = [];
-  for (let i = limit; i > offst; i--) {
-    data.push({
-      id: i,
-      name: "TEST" + i,
-      email: "test" + i + "@example.com",
-    });
-  }
-  return data;
-};
-
+import { defineComponent, reactive, ref, computed } from "vue";
+import TableLite from "vue3-table-lite";
 export default defineComponent({
-  name: 'ListeComp',
-  props: {
-    msg: String
-  },
+  name: "ListeComp",
   components: { TableLite },
   setup() {
+    const searchTerm = ref(""); // Search text
+    // Fake data
+    const data = reactive([]);
+    for (let i = 0; i < 126; i++) {
+      data.push({
+        id: i,
+        name: "TEST" + i,
+        email: "test" + i + "@example.com",
+      });
+    }
     // Table config
     const table = reactive({
-      isLoading: false,
       columns: [
         {
           label: "ID",
@@ -71,38 +51,24 @@ export default defineComponent({
           sortable: true,
         },
       ],
-      rows: [],
-      totalRecordCount: 0,
+      rows: computed(() => {
+        return data.filter(
+          (x) =>
+            x.email.toLowerCase().includes(searchTerm.value.toLowerCase()) ||
+            x.name.toLowerCase().includes(searchTerm.value.toLowerCase())
+        );
+      }),
+      totalRecordCount: computed(() => {
+        return table.rows.length;
+      }),
       sortable: {
         order: "id",
         sort: "asc",
       },
     });
-    /**
-     * Search Event
-     */
-    const doSearch = (offset, limit, order, sort) => {
-      table.isLoading = true;
-      setTimeout(() => {
-        table.isReSearch = offset == undefined ? true : false;
-        if (offset >= 10 || limit >= 20) {
-          limit = 20;
-        }
-        if (sort == "asc") {
-          table.rows = sampleData1(offset, limit);
-        } else {
-          table.rows = sampleData2(offset, limit);
-        }
-        table.totalRecordCount = 20;
-        table.sortable.order = order;
-        table.sortable.sort = sort;
-      }, 600);
-    };
-    // First get data
-    doSearch(0, 10, 'id', 'asc');
     return {
+      searchTerm,
       table,
-      doSearch,
     };
   },
 });
