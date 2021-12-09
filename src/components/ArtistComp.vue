e8ab0a943bda882e9fe7dbac024f6de8f72c4a1a
Lohier Anthony <anthony.lohier@etudiant.univ-rennes1.fr>
2021-12-09 17:57:49 +0100

    add back

---
+++ src/components/ArtistComp.vue
@@ -0,0 +1,253 @@
+<template>
+  <div v-if="currentArtist" class="edit-form float-right">
+    <h4>Artiste</h4>
+    <form>
+      <div class="form-group">
+        <label for="name">Nom</label>
+        <input
+          type="text"
+          class="form-control"
+          id="name"
+          v-model="currentArtist.fields.artistes"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="pays">Pays</label>
+        <input
+          type="text"
+          class="form-control"
+          id="pays"
+          v-model="currentArtist.fields.origine_pays1"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="salle">Salle</label>
+        <input
+          type="text"
+          class="form-control"
+          id="salle"
+          v-model="currentArtist.fields.premiere_salle"
+        />
+      </div>
+
+
+      <div class="form-group">
+        <label for="date">Date</label>
+        <input
+          type="text"
+          class="form-control"
+          id="date"
+          v-model="currentArtist.fields.premiere_date"
+        />
+      </div>
+
+      <div class="form-group">
+        <!-- <label><strong>Status:</strong></label> -->
+        <!-- {{ currentArtist.published ? "Published" : "Pending" }} -->
+        {{ message }}
+
+      </div>
+    </form>
+
+<!--     <button
+      class="badge badge-primary mr-2"
+      @click="updatePublished(false)"
+    >
+      UnPublish
+    </button>
+    <button
+      class="badge badge-primary mr-2"
+      @click="updatePublished(true)"
+    >
+      Publish
+    </button> -->
+
+    <button class="btn btn-danger" @click="deleteArtist">
+      Delete
+    </button>
+
+    <button type="submit" class="btn btn-success" @click="updateArtist">
+      Update
+    </button>
+  </div>
+ <!--
+  <div v-else>
+    <br />
+    <h4>Please click on a Ardddtist...</h4>
+  </div>-->
+</template>
+
+<script>
+import ArtistDataService from '../services/ArtistService';
+export default {
+  name: "artist",
+  props: ["artist"],
+  data() {
+    return {
+      currentArtist: null,
+      message: "",
+    };
+  },
+  watch: {
+    artist: function(artist) {
+      this.currentArtist = { ...artist };
+      this.message = "";
+    },
+  },
+  methods: {
+    /* updatePublished(status) {
+      ArtistDataService.update(this.currentArtist.key, {
+        published: status,
+      })
+        .then(() => {
+          //this.currentArtist.published = status;
+          this.message = "The status was updated successfully!";
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }, */
+    updateArtist() {
+      const data = {
+
+        /* name: this.currentArtist.name,
+        country: this.currentArtist.country,
+        premiere_date: this.currentArtist.premiere_date,
+        premiere_salle: this.currentArtist.premiere_salle,
+        geometry: this.currentArtist.geometry,
+        datasetid: this.currentArtist.datasetid,
+        recordid: this.currentArtist.recordid,
+        record_timestamp: this.currentArtist.record_timestamp, */
+        /* databasetid: this.currentArtist.databasetid,
+        fields: {
+            "spotify": "spotify:album:18Zy53OB9BCG0GtoGkQXv0",
+            "1ere_date_timestamp": 1101942000,
+            "cou_official_lang_code": "EN",
+            "cou_onu_code": "578",
+            "artistes": this.currentArtist.fields,
+            "cou_iso2_code": "NO",
+            "geo_point_2d": [
+                68.7958281693,
+                15.4107026868
+            ],
+            "cou_iso3_code": "NOR",
+            "1ere_salle": "Parc des Expositions - Hall 5",
+            "cou_is_receiving_quest": "Y",
+            "edition": "26èmes Rencontres Trans Musicales",
+            "cou_text_sp": "Noruega",
+            "1ere_date": "01-déc-04",
+            "cou_is_ilomember": "Y",
+            "annee": "2004",
+            "deezer": "2999681",
+            "cou_text_en": "Norway",
+            "origine_pays1": "Norvège"
+            }, */
+        /* geometry: {
+          "type": this.currentArtist.geometry,
+          "coordinates": [
+            15.4107026868,
+            68.7958281693
+          ]
+        },
+
+        record_timestamp: this.currentArtist.record_timestamp,
+        recordid: this.currentArtist.recordid, */
+
+        datasetid: this.currentArtist.datasetid,
+            fields: {
+              spotify: this.currentArtist.fields.spotify,
+              premiere_date_timestamp: this.currentArtist.fields.premiere_date_timestamp,
+              cou_official_lang_code: this.currentArtist.fields.cou_official_lang_code,
+              cou_onu_code: this.currentArtist.fields.cou_onu_code,
+              artistes: this.currentArtist.fields.artistes,
+              cou_iso2_code: this.currentArtist.fields.cou_iso2_code,
+              geo_point_2d: this.currentArtist.fields.geo_point_2d,
+              cou_iso3_code: this.currentArtist.fields.cou_iso3_code,
+              "1ere_salle": this.currentArtist.fields.premiere_salle,
+              cou_is_receiving_quest: this.currentArtist.fields.cou_is_receiving_quest,
+              edition: this.currentArtist.fields.edition,
+              cou_text_sp: this.currentArtist.fields.cou_text_sp,
+              "1ere_date": this.currentArtist.fields.premiere_date,
+              cou_is_ilomember: this.currentArtist.fields.cou_is_ilomember,
+              annee: this.currentArtist.fields.annee,
+              deezer: this.currentArtist.fields.deezer,
+              cou_text_en: this.currentArtist.fields.cou_text_en,
+              origine_pays1: this.currentArtist.fields.origine_pays1,
+            }, 
+              geometry: {
+                type: this.currentArtist.geometry.type,
+                coordinates: this.currentArtist.geometry.coordinates
+                /* {
+                  "0":item.child("/fields/geometry/coordinates/0").val(),
+                  "1":item.child("/fields/geometry/coordinates/1").val()
+                } */
+                
+            
+              },
+
+              record_timestamp: this.currentArtist.record_timestamp,
+              recordid: this.currentArtist.recordid
+            
+      };
+      ArtistDataService.update(this.currentArtist.key, data)
+        .then(() => {
+          this.message = "The artist was updated successfully!";
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    deleteArtist() {
+      ArtistDataService.delete(this.currentArtist.key)
+        .then(() => {
+          this.$emit("refreshList");
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+  mounted() {
+    this.message = "";
+    this.currentArtist = { ...this.artist }
+  },
+};
+</script>
+
+<style scoped lang="scss">
+$primary: crimson;
+
+
+h2{
+  margin: 20px 0 20px;
+  color: black;
+}
+label, h4 {
+  color: black;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: $primary;
+}
+.edit-form{
+    margin-left: 25%;
+}
+.vertical-center .form-control:focus {
+  border-color: #2554FF;
+  box-shadow: none;
+}
+.btn{
+    margin-top:10px;
+    margin-right:5px;
+}
+
+</style>
--- src/components/ListeComp.vue
+++ src/components/ListeComp.vue
@@ -1,89 +1,192 @@
 <template>
-  <div style="text-align: center">
-    <label>SearchBy:</label><input v-model="searchTerm" />
+  <div class="list row">
+    <div class="col-md-6">
+      <h4>Liste des artistes</h4>
+      <table class="table table-bordered float-left">
+        <thead>
+          <tr>
+            <th scope="col-md-5">Name</th>
+            <th scope="col-md-5">Pays</th>
+            <th scope="col-md-5">Date</th>
+            <th scope="col-md-5">Salle</th>
+          </tr>
+        </thead>
+         <tbody>
+
+            <tr class="table-item"
+          :class="{ active: index == currentIndex }"
+          v-for="(artist, index) in artists"
+          :key="index"
+          @click="setActiveArtist(artist, index)">
+              <th scope="row">{{artist.fields.artistes}}</th>
+              <td>{{artist.fields.origine_pays1}}</td>
+              <td>{{artist.fields.premiere_date}}</td>
+              <td>{{artist.fields.premiere_salle}}</td>
+            </tr>
+        
+        </tbody>
+      </table>
+      <!-- <ul class="table-group">
+        <li
+          class="list-group-item"
+          :class="{ active: index == currentIndex }"
+          v-for="(artist, index) in artists"
+          :key="index"
+          @click="setActiveArtist(artist, index)"
+        >
+          {{ artist.name }}
+        </li>
+        <li
+        class="list-group-item"
+          :class="{ active: index == currentIndex }"
+          v-for="(artist, index) in artists"
+          :key="index">
+          {{artist.record_timestamp}}
+        </li>
+      </ul> -->
+
+      <button class="btn btn-danger" @click="removeAllArtists">
+        Supprimer tout les artistes
+      </button>
+    </div>
+    <!-- update part -->
+    <div class="col-md-6">
+      <div v-if="currentArtist">
+        <artist-details
+          :artist="currentArtist"
+          @refreshList="refreshList"
+        />
+      </div>
+      <div v-else>
+        <br />
+        <h4 class="messageAjour">Cliquer sur un artiste pour le mettre à jour</h4>
+      </div>
+    </div>
   </div>
-  <table-lite
-    :is-static-mode="true"
-    :columns="table.columns"
-    :rows="table.rows"
-    :total="table.totalRecordCount"
-    :sortable="table.sortable"
-  ></table-lite>
 </template>
 
 <script>
-import { defineComponent, reactive, ref, computed } from "vue";
-import TableLite from "vue3-table-lite";
-export default defineComponent({
-  name: "ListeComp",
-  components: { TableLite },
-  setup() {
-    const searchTerm = ref(""); // Search text
-    // Fake data
-    const data = reactive([]);
-    for (let i = 0; i < 126; i++) {
-      data.push({
-        id: i,
-        name: "TEST" + i,
-        email: "test" + i + "@example.com",
-      });
-    }
-    // Table config
-    const table = reactive({
-      columns: [
-        {
-          label: "ID",
-          field: "id",
-          width: "3%",
-          sortable: true,
-          isKey: true,
-        },
-        {
-          label: "Name",
-          field: "name",
-          width: "10%",
-          sortable: true,
-        },
-        {
-          label: "Email",
-          field: "email",
-          width: "15%",
-          sortable: true,
-        },
-      ],
-      rows: computed(() => {
-        return data.filter(
-          (x) =>
-            x.email.toLowerCase().includes(searchTerm.value.toLowerCase()) ||
-            x.name.toLowerCase().includes(searchTerm.value.toLowerCase())
-        );
-      }),
-      totalRecordCount: computed(() => {
-        return table.rows.length;
-      }),
-      sortable: {
-        order: "id",
-        sort: "asc",
-      },
-    });
+import ArtistDataService from "../services/ArtistService";
+import ArtistDetails from "./ArtistComp";
+export default {
+  name: "artists-list",
+  components: { ArtistDetails },
+  data() {
     return {
-      searchTerm,
-      table,
+      artists: [],
+      currentArtist: null,
+      currentIndex: -1
     };
   },
-});
+  methods: {
+    onDataChange(items) {
+      let _artists = [];
+      items.forEach((item) => {
+        let key = item.key;
+        let data = item.val();
+        
+            _artists.push({
+            /* key: key,
+            name: item.child("/fields/artistes").val(),
+            country: item.child("/fields/origine_pays1").val(),
+            premiere_date: item.child("/fields/1ere_date").val(),
+            premiere_salle: item.child("/fields/1ere_salle").val(),
+            date: item.child("/fields/1ere_date").val(),
+            geometry: data.geometry,
+            datasetid: data.datasetid,
+            recordid: data.recordid,
+            record_timestamp: data.record_timestamp,
+            }); */
+            key: key,
+            datasetid: data.datasetid,
+            fields: {
+              spotify: item.child("/fields/spotify").val(),
+              premiere_date_timestamp: item.child("/fields/1ere_date_timestamp").val(),
+              cou_official_lang_code: item.child("/fields/cou_official_lang_code").val(),
+              cou_onu_code: item.child("/fields/cou_onu_code").val(),
+              artistes: item.child("/fields/artistes").val(),
+              cou_iso2_code: item.child("/fields/cou_iso2_code").val(),
+              geo_point_2d: {
+                "0":item.child("/fields/geo_point_2d/0").val(),
+                "1":item.child("/fields/geo_point_2d/1").val()
+              },
+              cou_iso3_code: item.child("/fields/cou_iso3_code").val(),
+              premiere_salle: item.child("/fields/1ere_salle").val(),
+              cou_is_receiving_quest: item.child("/fields/cou_is_receiving_quest").val(),
+              edition: item.child("/fields/edition").val(),
+              cou_text_sp: item.child("/fields/cou_text_sp").val(),
+              premiere_date: item.child("/fields/1ere_date").val(),
+              cou_is_ilomember: item.child("/fields/cou_is_ilomember").val(),
+              annee: item.child("/fields/annee").val(),
+              deezer: item.child("/fields/deezer").val(),
+              cou_text_en: item.child("/fields/cou_text_en").val(),
+              origine_pays1: item.child("/fields/origine_pays1").val()
+            }, 
+              geometry: {
+                type: item.child("/fields/geometry/type").val(),
+                coordinates: {
+                  "0":item.child("/fields/geometry/coordinates/0").val(),
+                  "1":item.child("/fields/geometry/coordinates/1").val()
+                }
+            
+              },
+
+              record_timestamp: data.record_timestamp,
+              recordid: data.recordid
+            
+          });
+      });
+      this.artists = _artists;
+    },
+    refreshList() {
+      this.currentArtist = null;
+      this.currentIndex = -1;
+    },
+    setActiveArtist(artist, index) {
+      this.currentArtist = artist;
+      this.currentIndex = index;
+      console.log(this.currentArtist);
+    },
+    removeAllArtists() {
+      ArtistDataService.deleteAll()
+        .then(() => {
+          this.refreshList();
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+  mounted() { 
+    //pour la version final mettre dernière edition des trans 42 ? 
+    ArtistDataService.getAll().orderByChild("/fields/edition").equalTo("26èmes Rencontres Trans Musicales").on("value", this.onDataChange);
+  },
+  beforeUnmount() {
+    ArtistDataService.getAll().off("value", this.onDataChange);
+  }
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 $primary: crimson;
 
+.list {
+  text-align: left;
+  max-width: 750px;
+  margin: auto;
+}
+.messageAjour{
+  margin-left:40px;
+}
+.btn-danger{
+  margin-bottom: 10px;
+}
 
 h2 {
   margin: 20px 0 20px;
   color: black;
 }
-label {
+label, h4 {
   color: black;
 }
 ul {
@@ -96,18 +199,6 @@ li {
 }
 a {
   color: $primary;
-}
-.form-control{
-    width: 50%;
-    margin-left: 25%;
-    margin-bottom: 20px;
-}
-.vertical-center .form-control:focus {
-  border-color: #2554FF;
-  box-shadow: none;
-}
-#array-rendering{
-  color:red;
 }
 
 </style>
