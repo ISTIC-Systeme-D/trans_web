8a05bef9e1c068e6b29fa70c79023466828cc0c3
Elodie <elodie.iantila-soanomena@etudiant.univ-rennes1.fr>
2021-12-10 09:56:27 +0100

    login +register+logout( modif: de App.vue,Home.vue,index.js,LoginComp.vue; ajout de Register.vue et RegisterComp.vue)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,8 +8,32 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import {ref, onBeforeMount} from "vue";
+import firebase from "../firebase";
+require("firebase/auth");
+//Pour le logout
 export default {
+  setup(){
+    const name = ref("");
+    onBeforeMount(() =>{
+      const user = firebase.app.auth().currentUser;
+      if(user){
+        name.value = user.email.split('@')[0];
+      }
+    });
+    const Logout = ()=>{
+      firebase.app
+          .auth()
+          .signOut()
+          .then(()=>console.log("Signed out"))
+          .catch(err=>alert(err.message));
+    }
+    return {
+      name,
+      Logout
+    }
+  },
+
   name: 'Home',
   components: {
     HelloWorld
